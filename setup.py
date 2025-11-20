a840100294960ec60133c11abda67dfded0d85e4
Yusuke_Kanazawa <c1005073@planet.kanazawa-it.ac.jp>
2025-11-20 20:37:43 +0900

    *.launch.py -> *launch.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'),
-            glob('launch/*.launch.py')),
+            glob('launch/*launch.py')),
         (os.path.join('share', package_name, 'rviz'),
             glob('rviz/*.rviz')),
     ],
