7cbe9ae97888d13871c2777f2fcb3eae219899f9
Yusuke_Kanazawa <c1005073@planet.kanazawa-it.ac.jp>
2025-11-20 20:33:02 +0900

    ros2_tao_pointpillarsのbboxをパブリッシュするスクリプトとrviz2を起動するlaunchファイル

---
+++ launch/rviz_pointpillars_launch.py
@@ -0,0 +1,29 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    rviz_config = os.path.join(
+        get_package_share_directory('reid_pillar_hf'),
+        'rviz',
+        'pointpillars.rviz'
+    )
+
+    namespace = 'reid_pillar_hf'
+
+    return LaunchDescription([
+        Node(
+            package='reid_pillar_hf',
+            executable='pub_pillar_bbox', 
+            output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config],
+            output='screen'
+        )
+    ])
